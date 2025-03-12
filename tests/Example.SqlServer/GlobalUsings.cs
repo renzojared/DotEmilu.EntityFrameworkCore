35bba631b36a8de4fc6bb665e6f835324e3359e2
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-12 00:15:03 -0500

    test(sql-server): global using

---
+++ tests/Example.SqlServer/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using DotEmilu.EntityFrameworkCore;
+global using Example.SqlServer;
+global using Example.SqlServer.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
