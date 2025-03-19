620732102c279f4153189e43ba60bad2c241b010
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-19 00:38:38 -0500

    feat: context user now support generic type for id for more flexibility

--- src/DotEmilu.EntityFrameworkCore/GlobalUsings.cs
+++ src/DotEmilu.EntityFrameworkCore/GlobalUsings.cs
@@ -1,5 +1,7 @@
 // Global using directives
 
+global using System.Reflection;
+global using DotEmilu.EntityFrameworkCore.Extensions;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
