31588c308d85bb189687fc17315cee2c254f1509
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-20 00:01:04 -0500

    refactor: change di container to separate register of interceptors

--- src/DotEmilu.EntityFrameworkCore/DiContainer.cs
+++ src/DotEmilu.EntityFrameworkCore/DiContainer.cs
@@ -1,14 +1,20 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace DotEmilu.EntityFrameworkCore;
 
 public static class DiContainer
 {
-    public static IServiceCollection AddDotEmiluInterceptors<TContextUser>(this IServiceCollection services)
-        where TContextUser : class, IContextUser
-        => services
-            .AddSingleton(TimeProvider.System)
-            .AddScoped<IContextUser, TContextUser>()
-            .AddScoped<ISaveChangesInterceptor, SoftDeleteInterceptor>()
-            .AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+    public static IServiceCollection AddSoftDeleteInterceptor(this IServiceCollection services)
+        => services.AddScoped<ISaveChangesInterceptor, SoftDeleteInterceptor>();
+
+    public static IServiceCollection AddAuditableEntityInterceptor<TContextUser, TUserKey>(
+        this IServiceCollection services)
+        where TContextUser : class, IContextUser<TUserKey>
+        where TUserKey : struct
+    {
+        services.TryAddSingleton(TimeProvider.System);
+        services.TryAddScoped<IContextUser<TUserKey>, TContextUser>();
+        return services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor<TUserKey>>();
+    }
 }
