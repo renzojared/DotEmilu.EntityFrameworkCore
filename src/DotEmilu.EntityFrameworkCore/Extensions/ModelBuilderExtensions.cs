555c83446142f0869bdbc90f6ab55e448666bbf2
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-21 09:53:15 -0500

    feat: introduce extension to register multiple configurations per array of types

--- src/DotEmilu.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
+++ src/DotEmilu.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -20,6 +20,31 @@ public static class ModelBuilderExtensions
         return modelBuilder;
     }
 
+    public static ModelBuilder ApplyBaseEntityConfiguration(this ModelBuilder modelBuilder,
+        MappingStrategy strategy = MappingStrategy.Tpc, bool enableRowVersion = false, params Type[] keyTypes)
+    {
+        if (keyTypes.Any(type => !type.IsValueType))
+            throw new ArgumentException("All keyTypes must be value types (structs).", nameof(keyTypes));
+
+        foreach (var keyType in keyTypes)
+        {
+            var baseEntityGenericType = typeof(IBaseEntity<>).MakeGenericType(keyType);
+
+            foreach (var entityType in modelBuilder.Model
+                         .GetEntityTypes()
+                         .Where(e => baseEntityGenericType
+                             .IsAssignableFrom(e.ClrType) && !e.ClrType.IsAbstract))
+            {
+                var configType = typeof(BaseEntityConfiguration<,>)
+                    .MakeGenericType(entityType.ClrType, keyType);
+                var configInstance = Activator.CreateInstance(configType, strategy, enableRowVersion);
+                modelBuilder.ApplyConfiguration((dynamic)configInstance!);
+            }
+        }
+
+        return modelBuilder;
+    }
+
     public static ModelBuilder ApplyBaseAuditableEntityConfiguration<TUserKey>(this ModelBuilder modelBuilder,
         MappingStrategy strategy = MappingStrategy.Tpc)
         where TUserKey : struct
@@ -33,6 +58,31 @@ public static class ModelBuilderExtensions
                 .MakeGenericType(entityType.ClrType, typeof(TUserKey));
             var configInstance = Activator.CreateInstance(configType, strategy);
             modelBuilder.ApplyConfiguration((dynamic)configInstance!);
+        }
+
+        return modelBuilder;
+    }
+
+    public static ModelBuilder ApplyBaseAuditableEntityConfiguration(this ModelBuilder modelBuilder,
+        MappingStrategy strategy = MappingStrategy.Tpc, params Type[] userKeyTypes)
+    {
+        if (userKeyTypes.Any(type => !type.IsValueType))
+            throw new ArgumentException("All userKeyTypes must be value types (structs).", nameof(userKeyTypes));
+
+        foreach (var userKeyType in userKeyTypes)
+        {
+            var baseAuditableEntityGenericType = typeof(IBaseAuditableEntity<>).MakeGenericType(userKeyType);
+
+            foreach (var entityType in modelBuilder.Model
+                         .GetEntityTypes()
+                         .Where(e => baseAuditableEntityGenericType
+                             .IsAssignableFrom(e.ClrType) && !e.ClrType.IsAbstract))
+            {
+                var configType = typeof(BaseAuditableEntityConfiguration<,>)
+                    .MakeGenericType(entityType.ClrType, userKeyType);
+                var configInstance = Activator.CreateInstance(configType, strategy);
+                modelBuilder.ApplyConfiguration((dynamic)configInstance!);
+            }
         }
 
         return modelBuilder;
